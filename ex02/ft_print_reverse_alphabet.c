6402b0a789a25d66105285fd9117bb7c042f1bc9
Bastien Guyot <bguyot@k1r1p8.42mulhouse.fr>
2021-11-13 10:35:24 +0100

    sheiss le 4

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:18:32 by bguyot            #+#    #+#             */
-/*   Updated: 2021/11/11 13:27:17 by bguyot           ###   ########.fr       */
+/*   Updated: 2021/11/13 10:30:03 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -16,7 +16,7 @@ void	ft_print_reverse_alphabet(void)
 	char	letter;
 
 	letter = 'z';
-	while (i <= 'a')
+	while (letter <= 'a')
 	{
 		write(1, &letter, 1);
 		letter--;
