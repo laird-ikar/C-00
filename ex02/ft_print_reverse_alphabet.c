a5809120d7e68b698346fd4397e7d5e2929eb485
Bastien Guyot <bguyot@k1r1p8.42mulhouse.fr>
2021-12-02 13:13:52 +0100

    correcting some bugs

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -6,9 +6,10 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:18:32 by bguyot            #+#    #+#             */
-/*   Updated: 2021/11/13 16:50:52 by bguyot           ###   ########.fr       */
+/*   Updated: 2021/12/02 13:10:52 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_print_reverse_alphabet(void)
--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,65 +6,31 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 16:42:55 by bguyot            #+#    #+#             */
-/*   Updated: 2021/11/12 07:15:50 by bguyot           ###   ########.fr       */
+/*   Updated: 2021/12/02 13:12:01 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	raise(int base, int power);
-int	loga10(int nb);
+void	ft_putnbr(int nb);
+void	ft_putchar(char c);
 
 void	ft_putnbr(int nb)
 {
-	char	outchar;
-	int		power;
-	int		first_digit;
-
 	if (nb < 0)
 	{
 		write(1, "-", 1);
-		nb *= -1;
+		if (nb <= -10)
+			ft_putnbr(nb / -10);
+		ft_putchar((-1 * (nb % 10)) + '0');
 	}
-	if (nb == 0)
-	{
-		write(1, "0", 1);
-	}
-	else
-	{
-		power = loga10(nb);
-		while (power >= 0)
-		{
-			first_digit = nb / raise(10, power);
-			outchar = first_digit + '0';
-			write(1, &outchar, 1);
-			nb -= first_digit * raise(10, power);
-			power--;
-		}
-	}
+	else if (nb >= 10)
+		ft_putnbr(nb / 10);
+	if (nb >= 0)
+		ft_putchar(nb % 10 + '0');
 }
 
-int	raise(int base, int power)
+void	ft_putchar(char c)
 {
-	if (power == 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (10 * raise(base, power - 1));
-	}
-}
-
-int	loga10(int nb)
-{
-	int	ln;
-
-	ln = 0;
-	while (nb >= 10)
-	{
-		nb /= 10;
-		ln++;
-	}
-	return (ln);
+	write(1, &c, 1);
 }
